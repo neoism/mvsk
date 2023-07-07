9bd17dd746e49c73a5b9c7d0f64418bbd44bb52f
neweos <65535758+neweos@users.noreply.github.com>
2023-07-08 01:23:12 +0800

    neweos created a commit :sparkles:

--- lib/liz.rb
+++ lib/liz.rb
@@ -2,7 +2,39 @@ require 'liz/version'
 require 'liz/token'
 require 'liz/lexer'
 
-# liz
+module Liz
+  module_function
+
+  def run_prompt
+    # InputStreamReader input = new InputStreamReader(System.in);
+    # BufferedReader reader = new BufferedReader(input);
+
+    while buf = Readline.readline('> ', true)
+      break if buf.strip.eql?('exit')
+      # run(line); //> reset-had-error
+      # hadError = false;
+    end
+  end
+
+  def run_file(path)
+    if File.exist?(path) && File.file?(path)
+      puts 'fiasd'
+    else
+      warn "No such file #{path}"
+    end
+
+    # run(File.read(path))
+    # # if (hadError) System.exit(65);
+    # # if (hadRuntimeError) System.exit(70);
+  end
+
+  def run(code)
+    lexer = Lexer.new(code)
+    tokens = lexer.scan_tokens
+    tokens.each { |token| puts token }
+  end
+end
+
 # ["And",
 #  "Class",
 #  "Else",
